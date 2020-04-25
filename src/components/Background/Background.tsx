58ac802e0621782f432f2ea0342d33f908e6de0c
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-04-25 19:25:51 -0400

    adedd background color, flex properties, and dimensions

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+
 interface Props {
   children: React.ReactNode;
 }
 
+const useStyles = makeStyles(({ palette }: Theme) =>
+  createStyles({
+    background: {
+      backgroundColor: palette.primary.main,
+      width: '100%',
+      height: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  })
+);
+
 const Background: React.FC<Props> = ({ children }) => {
-  return <div>{children}</div>;
+  const classes = useStyles();
+  return <div className={classes.background}>{children}</div>;
 };
 
 export default Background;
