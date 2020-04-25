d3f65e327556675742bba11d20d6e0bf144fbf5e
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-04-25 16:29:46 -0400

    created custom theme

---
+++ src/styles/theme.tsx
@@ -0,0 +1,19 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import red from '@material-ui/core/colors/red';
+
+const CustomTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#100452',
+    },
+    secondary: {
+      main: '#05001f',
+    },
+    error: {
+      main: red.A700,
+    },
+  },
+  spacing: 4,
+});
+
+export default CustomTheme;
