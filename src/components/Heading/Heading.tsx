9342ed63e363ec0ff555a0760ebef4a7f2ac7e1d
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-04-26 16:19:48 -0400

    added custom styles

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import { Box, Typography } from '@material-ui/core';
 
 interface Props {
   title: string;
 }
 
+const useStyles = makeStyles(({ palette, spacing }: Theme) =>
+  createStyles({
+    heading: {
+      backgroundColor: palette.secondary.main,
+      color: 'white',
+      padding: spacing(4),
+      fontWeight: 800,
+      letterSpacing: '-1px',
+    },
+  })
+);
+
 const Heading: React.FC<Props> = ({ title }) => {
+  const classes = useStyles();
   return (
     <Box mb={4}>
-      <Typography variant="h4" component="h1">
+      <Typography variant="h4" component="h1" className={classes.heading}>
         {title}
       </Typography>
     </Box>
