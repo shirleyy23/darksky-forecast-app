78a52f4aa532f1c28971435dd81f0eaae81d0e24
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-04-27 17:40:03 -0400

    added dependencies for custom button

---
+++ src/components/CustomButton/CustomButton.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {
+  Link as RouterLink,
+  LinkProps as RouterLinkProps,
+} from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import { Omit } from '@material-ui/types';
+import { makeStyles } from '@material-ui/core/styles';
