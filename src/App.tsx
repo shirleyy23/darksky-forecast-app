c8570e48253fdc5de7857a6f1dd144e4918d43eb
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-04-27 15:53:01 -0400

    imported Router to handle routing in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import Router from './components/Router/Router';
+
 const App: React.FC<{}> = () => {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <Router />
+    </div>
+  );
 };
 
 export default App;
