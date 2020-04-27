3bdeed4b375c8a1ecca0fa1cc6589a4eaa088eb9
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-04-27 15:52:38 -0400

    created Router to link to different pages

---
+++ src/components/Router/Router.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import Home from '../../Pages/Home/Home';
+import Forecast from '../../Pages/Forecast/Forecast';
+
+const Router: React.FC<{}> = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/forecast" component={Forecast} />
+        <Redirect to="/" />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
