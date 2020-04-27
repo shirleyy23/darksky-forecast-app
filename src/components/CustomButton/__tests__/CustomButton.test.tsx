2ee30c748dc47be72f85e36a7854c600311b3726
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-04-27 18:31:48 -0400

    added test with render function

---
+++ src/components/CustomButton/__tests__/CustomButton.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import CustomButton from '../CustomButton';
+
+type CustomButtonProps = React.ComponentProps<typeof CustomButton>;
+
+const renderCustomButton = (props: Partial<CustomButtonProps> = {}) => {
+  const defaultProps: CustomButtonProps = {
+    content: 'SEARCH',
+    link: '/forecast',
+  };
+  return render(
+    <BrowserRouter>
+      <CustomButton {...defaultProps} {...props} />
+    </BrowserRouter>
+  );
+};
