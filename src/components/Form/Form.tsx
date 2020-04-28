b7cc6010a7503f75cbf278b49da5a40c2240021a
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-04-28 16:12:01 -0400

    added basic styling for form components

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -2,3 +2,15 @@ import React from 'react';
 import { Box, Grid, TextField, Typography } from '@material-ui/core';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import CustomButton from '../CustomButton/CustomButton';
+
+const useStyles = makeStyles(({ palette, spacing }: Theme) =>
+  createStyles({
+    root: {
+      color: palette.secondary.main,
+      padding: spacing(5),
+    },
+    input: {
+      width: '100%',
+    },
+  })
+);
