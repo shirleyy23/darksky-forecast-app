147c3f940062c268595b3ee3995dd6d355425e0a
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-04-28 20:27:24 -0400

    included Redux provider with store

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,17 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 import App from './App';
 import CustomTheme from './styles/theme';
+import { Provider } from 'react-redux';
+import store from './Store';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={CustomTheme}>
-      <CssBaseline />
-      <App />
+      <Provider store={store}>
+        <CssBaseline />
+        <App />
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
