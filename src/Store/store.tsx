613d61d12ecc2e82190f95361656e00e2702a030
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-04-28 21:05:26 -0400

    created directory for store and changed paths to link to that store

--- src/Store/store.tsx
+++ src/Store/store.tsx
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import rootReducer from './Reducers/rootReducer';
+import rootReducer from '../Reducers/rootReducer';
 
 const store = createStore(
   rootReducer,
