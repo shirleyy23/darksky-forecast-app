48a5adcbc8b0a3d21d840a4aa49813c4d2748aa0
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-04-29 11:28:26 -0400

    added location reducer with default state

---
+++ src/Reducers/locationReducer.tsx
@@ -0,0 +1,33 @@
+import { GET_LOCATION, GetLocationTypes, Location } from '../Store/Types/types';
+
+export const initialState = {
+  latitude: -80.89343,
+  longitude: 40.89722,
+  data: {
+    timezone: '',
+    summary: '',
+    icon: '',
+    precipProbability: 0,
+    tempearture: 0,
+    windSpeed: 0,
+    uvIndex: 0,
+  },
+};
+const locationReducer = (
+  state = initialState,
+  action: GetLocationTypes
+): Location => {
+  switch (action.type) {
+    case GET_LOCATION:
+      return {
+        ...state,
+        latitude: action.payload.latitude,
+        longitude: action.payload.longitude,
+        data: action.payload.data,
+      };
+    default:
+      return state;
+  }
+};
+
+export default locationReducer;
