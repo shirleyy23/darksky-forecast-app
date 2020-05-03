16111ef1b2ae690bfe7857d4791d24da211db7bd
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-03 15:10:53 -0400

    imported custom render method to account for store and router in tests

--- src/components/Router/__tests__/Router.test.tsx
+++ src/components/Router/__tests__/Router.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
-import { render, fireEvent } from '@testing-library/react';
+
 import '@testing-library/jest-dom/extend-expect';
 import App from '../../../App';
+
+import { render, fireEvent, screen } from '../../../Store/Test/test-utils';
 
 test('full app rendering/navigating', () => {
   const history = createMemoryHistory();
