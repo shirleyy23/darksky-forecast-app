65f35b07d962112c0bb860f6cd5bd41d8f721b2a
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-03 20:09:39 -0400

    added test to check that button is disabled for incorrect form submission

--- src/components/CustomButton/__tests__/CustomButton.test.tsx
+++ src/components/CustomButton/__tests__/CustomButton.test.tsx
@@ -9,6 +9,7 @@ const renderCustomButton = (props: Partial<CustomButtonProps> = {}) => {
   const defaultProps: CustomButtonProps = {
     content: 'SEARCH',
     link: '/forecast',
+    disabled: false,
   };
   return render(
     <BrowserRouter>
@@ -32,5 +33,11 @@ describe('<CustomButton />', () => {
     const link = '/';
     const { container } = renderCustomButton({ link });
     expect(container.firstChild).toHaveClass('MuiButton-containedSecondary');
+  });
+
+  test('<CustomButton /> with disabled prop prevents users from clicking button', () => {
+    const disabled = true;
+    const { container } = renderCustomButton({ disabled });
+    expect(container.firstChild).toHaveClass('Mui-disabled');
   });
 });
