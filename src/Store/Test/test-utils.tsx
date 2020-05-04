a504de3eeed0f7992aeca2cafd8abf487048ad4e
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-04 16:09:52 -0400

    updated test render function to accept different routes

--- src/Store/Test/test-utils.tsx
+++ src/Store/Test/test-utils.tsx
@@ -3,8 +3,7 @@ import React, { ReactElement } from 'react';
 import { render as rtlRender } from '@testing-library/react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { initialState, locationReducer } from '../../Reducers/locationReducer';
-import { Location } from '../Types/types';
+import { initialState } from '../../Reducers/locationReducer';
 import { RootState, rootReducer } from '../../Reducers/rootReducer';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
@@ -15,17 +14,18 @@ const defaultData = {
   },
 };
 
-const history = createMemoryHistory();
-
 function render(
   ui: ReactElement,
   {
     initialState: RootState = defaultData,
     store = createStore(rootReducer, defaultData),
+    route = '/',
     ...renderOptions
   } = {}
 ) {
   function Wrapper({ children }: any) {
+    const history = createMemoryHistory();
+    history.push(route);
     return (
       <Provider store={store}>
         <Router history={history}>{children}</Router>
@@ -35,8 +35,6 @@ function render(
   return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
 }
 
-// re-export everything
 export * from '@testing-library/react';
 
-// override render method
 export { render };
