10d8523d3c181a2568ec9f0c1381ad0ba97fc29c
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-05 01:04:16 -0400

    added text to check if redux defaults are loaded

--- src/components/Form/__tests__/Form.test.tsx
+++ src/components/Form/__tests__/Form.test.tsx
@@ -4,3 +4,11 @@ import { Route } from 'react-router-dom';
 import '@testing-library/jest-dom/extend-expect';
 import Form from '../Form';
 import App from '../../../App';
+
+test('can render with redux with defaults', () => {
+  const { getByTestId } = render(<Form />);
+  const latitude = getByTestId('latitude');
+  const longitude = getByTestId('longitude');
+  expect(latitude).toHaveValue('-80.89343');
+  expect(longitude).toHaveValue('40.89722');
+});
