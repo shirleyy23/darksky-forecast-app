f78e79c81da980e0f0347c881b463922eb7d3364
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-05 01:05:54 -0400

    added tests to check for incorrect latitude and longitude values

--- src/components/Form/__tests__/Form.test.tsx
+++ src/components/Form/__tests__/Form.test.tsx
@@ -22,3 +22,23 @@ test('Can change input values', () => {
   expect(latitude).toHaveValue('35.3134');
   expect(longitude).toHaveValue('24.9048');
 });
+
+test('Error message is shown with an incorrect latitude value', () => {
+  const { getByTestId, container } = render(<Form />);
+  const latitude = getByTestId('latitude');
+  fireEvent.change(latitude, { target: { value: '40.39s' } });
+  expect(latitude).toHaveValue('40.39s');
+  const helperText = container.querySelector('#latitude-helper-text');
+  expect(helperText).toBeTruthy();
+  expect(helperText).toHaveTextContent('Please enter a number');
+});
+
+test('Error message is shown with an incorrect longitude value', () => {
+  const { getByTestId, container } = render(<Form />);
+  const longitude = getByTestId('longitude');
+  fireEvent.change(longitude, { target: { value: '20..4' } });
+  expect(longitude).toHaveValue('20..4');
+  const helperText = container.querySelector('#longitude-helper-text');
+  expect(helperText).toBeTruthy();
+  expect(helperText).toHaveTextContent('Please enter a number');
+});
