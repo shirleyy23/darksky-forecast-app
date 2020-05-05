d7ae08114d695b617c700f0dbeda2b6a46a3fd57
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-05 01:03:29 -0400

    added dependencies for form testing

---
+++ src/components/Form/__tests__/Form.test.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render, fireEvent } from '../../../Store/Test/test-utils';
+import { Route } from 'react-router-dom';
+import '@testing-library/jest-dom/extend-expect';
+import Form from '../Form';
+import App from '../../../App';
