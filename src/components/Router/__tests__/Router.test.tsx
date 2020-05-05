be0166912385623cddcbe7464579b400ca1fb8ea
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-05 01:01:05 -0400

    removed screen and history methods

--- src/components/Router/__tests__/Router.test.tsx
+++ src/components/Router/__tests__/Router.test.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { Router } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
 
 import '@testing-library/jest-dom/extend-expect';
 import App from '../../../App';
 
-import { render, fireEvent, screen } from '../../../Store/Test/test-utils';
+import { render, fireEvent } from '../../../Store/Test/test-utils';
 
 test('full app rendering/navigating', () => {
-  const history = createMemoryHistory();
   const { getByText } = render(<App />);
 
   expect(getByText('Weather App')).toBeInTheDocument();
