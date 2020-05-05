2f48a5ffa737768f1636432829f8676681ea732b
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-05 01:11:07 -0400

    updated test to use custom render function and react router methods

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+
+import { render, fireEvent } from './Store/Test/test-utils';
+
+import '@testing-library/jest-dom/extend-expect';
+
+import { Route } from 'react-router-dom';
 import App from './App';
 
 test('renders learn react link', () => {
