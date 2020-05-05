1fec68f9418a8556f707a7f8db26302ffa249944
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-05 01:11:42 -0400

    added test to check that home page renders correctly on page load

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,6 +7,9 @@ import '@testing-library/jest-dom/extend-expect';
 import { Route } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+describe('<App />', () => {
+  test('home page is rendered correctly', () => {
+    const { getByText } = render(<App />);
+    expect(getByText('Weather App')).toBeInTheDocument();
+  });
 });
