ee9dd9b7a5aaccee0cc3e2b958d1c845a63a2b46
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-06 14:43:47 -0400

    created base layout

--- src/Pages/Forecast/Forecast.tsx
+++ src/Pages/Forecast/Forecast.tsx
@@ -5,7 +5,17 @@ import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Heading from '../../components/Heading/Heading';
 
 const Forecast: React.FC<{}> = () => {
-  return <h2>Forecast</h2>;
+  return (
+    <Background>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper>
+            <Heading title="Forecast" />
+          </Paper>
+        </Grid>
+      </Grid>
+    </Background>
+  );
 };
 
 export default Forecast;
