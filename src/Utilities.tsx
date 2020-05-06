b1869c04b089353cf4ff5f9979b100da02c59a11
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-06 18:35:56 -0400

    added function to convert numbers to strings with units

--- src/Utilities.tsx
+++ src/Utilities.tsx
@@ -4,3 +4,10 @@ export const convertToPercentage = (value: number): string => {
   const formattedValue = `${value * 100}%`;
   return formattedValue;
 };
+
+//Convert numbers to string with units
+
+export const convertWithUnit = (value: number, unit: string): string => {
+  const formattedValue = `${value} ${unit}`;
+  return formattedValue;
+};
