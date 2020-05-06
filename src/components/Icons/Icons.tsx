fcbd6247ddeb6d68434f17149f5da59859df1551
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-06 17:25:05 -0400

    imported Font Awesome component and icon prop for type definitions

--- src/components/Icons/Icons.tsx
+++ src/components/Icons/Icons.tsx
@@ -1,3 +1,5 @@
 import React from 'react';
 import { Box, Typography } from '@material-ui/core';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
