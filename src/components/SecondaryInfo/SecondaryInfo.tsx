1381e7ac89394823aa9fd79ed8f95a69123bc1fd
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-06 17:02:16 -0400

    imported state and type definitions

---
+++ src/components/SecondaryInfo/SecondaryInfo.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { connect, ConnectedProps } from 'react-redux';
+import { RootState } from '../../Reducers/rootReducer';
+import { Location } from '../../Store/Types/types';
