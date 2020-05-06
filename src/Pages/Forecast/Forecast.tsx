24368337de172b65b95fd16583bf920a2a27aaee
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-06 14:41:52 -0400

    imported components to create layout

--- src/Pages/Forecast/Forecast.tsx
+++ src/Pages/Forecast/Forecast.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import Background from '../../components/Background/Background';
+import { Grid, Paper } from '@material-ui/core';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Heading from '../../components/Heading/Heading';
 
 const Forecast: React.FC<{}> = () => {
   return <h2>Forecast</h2>;
