284faa4bf021b129e02b6d7fe3f82a467cf381e6
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-06 18:37:45 -0400

    formatted precipitation and wind speed values using helper functions

--- src/components/SecondaryInfo/SecondaryInfo.tsx
+++ src/components/SecondaryInfo/SecondaryInfo.tsx
@@ -5,6 +5,7 @@ import { Location } from '../../Store/Types/types';
 import { Box } from '@material-ui/core';
 import Icons from '../Icons/Icons';
 import { faCloudRain, faSun, faWind } from '@fortawesome/free-solid-svg-icons';
+import { convertToPercentage, convertWithUnit } from '../../Utilities';
 
 const mapState = (state: RootState): Location => ({
   latitude: state.location.latitude,
@@ -19,6 +20,10 @@ type PropsFromRedux = ConnectedProps<typeof connector>;
 type Props = PropsFromRedux;
 const SecondaryInfo: React.FC<Props> = ({ data }: Props) => {
   const { uvIndex, windSpeed, precipProbability } = data;
+
+  const formattedPrecipProbability = convertToPercentage(precipProbability);
+
+  const formattedWindSpeed = convertWithUnit(windSpeed, 'km/h');
 
   return <Box flexWrap="wrap" display="flex" component="section" mb={5} />;
 };
