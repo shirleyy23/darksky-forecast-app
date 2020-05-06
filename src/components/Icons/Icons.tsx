7f73873490b4c549fceeb13e59690d130ba82ac7
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-06 17:25:37 -0400

    added type definitions for props

--- src/components/Icons/Icons.tsx
+++ src/components/Icons/Icons.tsx
@@ -3,3 +3,9 @@ import { Box, Typography } from '@material-ui/core';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
+
+interface Props {
+  title: string;
+  value: string;
+  icon: IconProp;
+}
