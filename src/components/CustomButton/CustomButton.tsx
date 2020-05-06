2d03fb275687994d5415ddd4c2cfac3d27f9cb55
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-06 14:12:57 -0400

    removed unused link prop imports

--- src/components/CustomButton/CustomButton.tsx
+++ src/components/CustomButton/CustomButton.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
-import {
-  Link as RouterLink,
-  LinkProps as RouterLinkProps,
-} from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
-import { Omit } from '@material-ui/types';
 import { makeStyles } from '@material-ui/core/styles';
 
 interface Props {
