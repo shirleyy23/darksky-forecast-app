df38b02c14e86f9a770d76f3245075490127c9fc
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-06 15:07:50 -0400

    mapped state to props with types and connector

--- src/components/PrimaryInfo/PrimaryInfo.tsx
+++ src/components/PrimaryInfo/PrimaryInfo.tsx
@@ -1,1 +1,19 @@
 import React from 'react';
+import { connect, ConnectedProps } from 'react-redux';
+
+import { RootState } from '../../Reducers/rootReducer';
+import { Location } from '../../Store/Types/types';
+
+const mapState = (state: RootState): Location => ({
+  latitude: state.location.latitude,
+  longitude: state.location.longitude,
+  data: state.location.data,
+});
+
+const connector = connect(mapState);
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+type Props = PropsFromRedux;
+
+export default connector(PrimaryInfo);
