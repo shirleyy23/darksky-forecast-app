fdfebb2c3ada85ffd0d91fece7a293db7ecaa07f
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-06 17:44:35 -0400

    added base layout with icon and hedings

--- src/components/Icons/Icons.tsx
+++ src/components/Icons/Icons.tsx
@@ -9,3 +9,20 @@ interface Props {
   value: string;
   icon: IconProp;
 }
+
+const Icons: React.FC<Props> = ({ title, icon, value }) => {
+  const classes = useStyles();
+  return (
+    <Box component="article" className={classes.container}>
+      <FontAwesomeIcon size="3x" className={classes.icon} icon={icon} />
+      <Typography component="h4" variant="h4" color="secondary">
+        {value}
+      </Typography>
+      <Typography component="h5" variant="subtitle1">
+        {title}
+      </Typography>
+    </Box>
+  );
+};
+
+export default Icons;
