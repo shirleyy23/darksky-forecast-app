f5909812ce33bc74a7d044badd9c64fcde2e6f7a
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-06 11:36:36 -0400

    used env variables to create path to connect to API endpoint

--- src/Lambda/weather.ts
+++ src/Lambda/weather.ts
@@ -7,4 +7,10 @@ interface APIResponse {
 
 export async function handler(event: any): Promise<APIResponse> {
   const data = JSON.parse(event.body);
+
+  const { latitude, longitude } = data;
+
+  const { API_PATH, API_KEY } = process.env;
+
+  const url = `${API_PATH}${API_KEY}/${latitude}, ${longitude}/?units=si`;
 }
