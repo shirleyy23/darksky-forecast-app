d55aff63fdb774a4e87e9735adf34d63aa103497
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-06 14:45:36 -0400

    added styles for grid container

--- src/Pages/Forecast/Forecast.tsx
+++ src/Pages/Forecast/Forecast.tsx
@@ -4,10 +4,20 @@ import { Grid, Paper } from '@material-ui/core';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Heading from '../../components/Heading/Heading';
 
+const useStyles = makeStyles(({ palette }: Theme) =>
+  createStyles({
+    root: {
+      width: '60%',
+      color: palette.secondary.main,
+    },
+  })
+);
+
 const Forecast: React.FC<{}> = () => {
+  const classes = useStyles();
   return (
     <Background>
-      <Grid container spacing={3}>
+      <Grid container spacing={3} className={classes.root}>
         <Grid item xs={12}>
           <Paper>
             <Heading title="Forecast" />
