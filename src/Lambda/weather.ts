42e1f4109b2f509db9ac373faa46ed4ddcf6ea69
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-06 11:37:06 -0400

    created try/catch to handle response from Dark Sky endpoint

--- src/Lambda/weather.ts
+++ src/Lambda/weather.ts
@@ -13,4 +13,20 @@ export async function handler(event: any): Promise<APIResponse> {
   const { API_PATH, API_KEY } = process.env;
 
   const url = `${API_PATH}${API_KEY}/${latitude}, ${longitude}/?units=si`;
+
+  try {
+    const response = await axios.get(url, {
+      headers: { Accept: 'application/json' },
+    });
+    const data = response.data;
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ msg: err.message }),
+    };
+  }
 }
