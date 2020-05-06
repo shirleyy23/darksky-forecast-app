05f5a9a1916311a1ba79dd9fceec26cf8429143c
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-06 17:24:08 -0400

    imported Material UI typography component

--- src/components/Icons/Icons.tsx
+++ src/components/Icons/Icons.tsx
@@ -1,3 +1,3 @@
 import React from 'react';
-import { Box } from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
