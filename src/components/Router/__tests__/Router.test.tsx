86843a3ef3a2fbba609c4573e70ba3e0a7978b9e
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-11 12:00:35 -0400

    updated router test to use mock data to test post and get

--- src/components/Router/__tests__/Router.test.tsx
+++ src/components/Router/__tests__/Router.test.tsx
@@ -3,14 +3,27 @@ import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import App from '../../../App';
 
-import { render, fireEvent } from '../../../Store/Test/test-utils';
+import {
+  render,
+  fireEvent,
+  waitFor,
+  screen,
+} from '../../../Store/Test/test-utils';
 
-test('full app rendering/navigating', () => {
-  const { getByText } = render(<App />);
+import axios from 'axios';
+
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+test('full app rendering/navigating', async () => {
+  const { getByText, debug } = render(<App />);
 
   expect(getByText('Weather App')).toBeInTheDocument();
 
   fireEvent.click(getByText(/SEARCH/i));
 
-  expect(getByText('Forecast')).toBeInTheDocument();
+  expect(getByText('Loading...')).toBeInTheDocument();
+
+  await waitFor(() => screen.getByText('Forecast'));
+
+  expect(mockedAxios.post).toHaveBeenCalledTimes(1);
 });
