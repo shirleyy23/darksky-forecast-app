012dfb41308190ae1dcc8cc675de23472852c8a7
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-11 12:00:05 -0400

    added mocks for post method

---
+++ src/__mocks__/axios.tsx
@@ -0,0 +1,19 @@
+export default {
+  post: jest.fn(() =>
+    Promise.resolve({
+      data: {
+        latitude: '23',
+        longitude: '23',
+        timezone: 'Toronto/Americas',
+        data: {
+          summary: 'Cloudy',
+          icon: 'sun',
+          precipProbability: 0.2,
+          temperature: 25.3,
+          windSpeed: 4.99,
+          uvIndex: 3,
+        },
+      },
+    })
+  ),
+};
