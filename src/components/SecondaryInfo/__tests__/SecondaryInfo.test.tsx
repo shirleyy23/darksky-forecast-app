86beca15ccbcaf63e13dc265c7b078fc76db3d6d
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-11 14:48:36 -0400

    added click event to trigger POST method

--- src/components/SecondaryInfo/__tests__/SecondaryInfo.test.tsx
+++ src/components/SecondaryInfo/__tests__/SecondaryInfo.test.tsx
@@ -1,3 +1,12 @@
 import React from 'react';
 import { render, fireEvent, waitFor } from '../../../Store/Test/test-utils';
 import App from '../../../App';
+
+test('<SecondaryInfo /> renders correctly', async () => {
+  const { getByTestId, getByText, getAllByTestId } = render(<App />);
+  const button = getByTestId('link');
+  fireEvent.click(button);
+  expect(getByText('Loading...')).toBeInTheDocument();
+  await waitFor(() => getByText('Forecast'));
+  const secondaryInfo = getByTestId('secondary-info');
+});
