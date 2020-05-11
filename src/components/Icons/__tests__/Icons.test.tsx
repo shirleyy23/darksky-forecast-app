e3d6a3780f4c7ada776980127fcb89869bf7eb39
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-11 13:45:45 -0400

    added test to check correct content and classes are rendered

--- src/components/Icons/__tests__/Icons.test.tsx
+++ src/components/Icons/__tests__/Icons.test.tsx
@@ -2,3 +2,18 @@ import React from 'react';
 import { render } from '../../../Store/Test/test-utils';
 import Icons from '../Icons';
 import { faWind } from '@fortawesome/free-solid-svg-icons';
+
+test('<Icons />', () => {
+  const { getByTestId, container } = render(
+    <Icons title="Wind Speed" value={2.56} icon={faWind} />
+  );
+
+  const title = getByTestId('icon-title');
+  const value = getByTestId('icon-value');
+
+  expect(container.querySelector('svg')).toHaveClass(
+    'svg-inline--fa fa-wind fa-w-16 fa-4x '
+  );
+  expect(title).toHaveTextContent('Wind Speed');
+  expect(value).toHaveTextContent('2.56');
+});
