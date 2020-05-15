01ae9abc32966adc69694680e554a4128e7dd11b
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-15 16:10:11 -0400

    changed formatting of api state action type

--- src/Actions/updateAPIState.tsx
+++ src/Actions/updateAPIState.tsx
@@ -1,8 +1,12 @@
-import { APIState, GET_APISTATE, GetAPIStateTypes } from '../Store/Types/types';
+import {
+  APIState,
+  GET_API_STATE,
+  GetAPIStateTypes,
+} from '../Store/Types/types';
 
 const updateAPIState = (updatedAPIState: APIState): GetAPIStateTypes => {
   return {
-    type: GET_APISTATE,
+    type: GET_API_STATE,
     payload: updatedAPIState,
   };
 };
