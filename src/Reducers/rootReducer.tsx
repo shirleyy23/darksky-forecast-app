69dafd879255889ee1261f2127d81dbb647eeb99
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-15 21:42:48 -0400

    included form submit reducer in root state

--- src/Reducers/rootReducer.tsx
+++ src/Reducers/rootReducer.tsx
@@ -4,9 +4,12 @@ import locationReducer from './locationReducer';
 
 import apiStateReducer from './apiStateReducer';
 
+import formSubmitReducer from './formSubmitReducer';
+
 export const rootReducer = combineReducers({
   location: locationReducer,
   apiState: apiStateReducer,
+  formSubmit: formSubmitReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
