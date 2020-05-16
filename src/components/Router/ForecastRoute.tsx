0d9c873809147b53264d5e32563e06fa18329452
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-15 22:16:45 -0400

    mapped state and dispatch to props

--- src/components/Router/ForecastRoute.tsx
+++ src/components/Router/ForecastRoute.tsx
@@ -5,3 +5,19 @@ import { RootState } from '../../Reducers/rootReducer';
 import { connect, ConnectedProps } from 'react-redux';
 import { withRouter } from 'react-router';
 import Forecast from '../../Pages/Forecast/Forecast';
+
+const mapState = (state: RootState): CombinedCustomTypes => ({
+  latitude: state.location.latitude,
+  longitude: state.location.longitude,
+  data: state.location.data,
+  loading: state.apiState.loading,
+  success: state.apiState.success,
+  fail: state.apiState.fail,
+  isSubmitted: state.formSubmit.isSubmitted,
+});
+
+const connector = connect(mapState);
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+type Props = PropsFromRedux;
