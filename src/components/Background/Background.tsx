87544ebc97b9726159223afcf88a288bcf340ce7
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-16 23:13:51 -0400

    moved Background style to separate file

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -1,27 +1,6 @@
 import React from 'react';
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import { useStyles } from './Styles/Style';
 import { ChildrenProps } from '../../types';
-
-const useStyles = makeStyles(({ palette, spacing }: Theme) =>
-  createStyles({
-    container: {
-      backgroundColor: palette.primary.main,
-      width: '100%',
-      height: '100vh',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      position: 'relative',
-    },
-    '@media (max-width: 680px)': {
-      container: {
-        minHeight: '100vh',
-        height: 'auto',
-        padding: spacing(4),
-      },
-    },
-  })
-);
 
 const Background: React.FC<ChildrenProps> = ({ children }) => {
   const classes = useStyles();
