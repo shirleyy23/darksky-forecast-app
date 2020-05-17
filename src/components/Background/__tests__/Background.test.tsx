f7a72f954a851728a2f3673199ad73682c6b09c7
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-17 19:06:41 -0400

    create custom render function that includes children prop

--- src/components/Background/__tests__/Background.test.tsx
+++ src/components/Background/__tests__/Background.test.tsx
@@ -1,3 +1,17 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import Background from '../Background';
+
+type BackgroundProps = React.ComponentProps<typeof Background>;
+
+const renderBackground = (props: Partial<BackgroundProps> = {}) => {
+  const exampleComponent = (
+    <div>
+      <p>Example Component</p>
+    </div>
+  );
+  const defaultProps: BackgroundProps = {
+    children: exampleComponent,
+  };
+  return render(<Background {...defaultProps} {...props} />);
+};
