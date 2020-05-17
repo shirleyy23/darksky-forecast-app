5fbc2de95d7e4f5c4a23819c2750f769bc6761cb
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-16 20:56:32 -0400

    added styles with media queries

--- src/components/ContentWrapper/ContentWrapper.tsx
+++ src/components/ContentWrapper/ContentWrapper.tsx
@@ -6,3 +6,21 @@ import ExternalLink from '../ExternalLink/ExternalLink';
 interface Props {
   children: React.ReactNode;
 }
+
+const useStyles = makeStyles(({ spacing, palette }: Theme) =>
+  createStyles({
+    root: {
+      padding: spacing(5),
+      color: palette.secondary.main,
+    },
+    paragraph: {
+      paddingTop: spacing(1),
+    },
+    '@media (max-width: 499px)': {
+      paragraph: {
+        paddingTop: spacing(4),
+        textAlign: 'left',
+      },
+    },
+  })
+);
