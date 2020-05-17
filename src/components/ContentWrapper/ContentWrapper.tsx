6572c47edec8716d813d0c85252b1fb1254a7f0a
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-16 20:55:26 -0400

    imported Material UI components and style methods, and React components

---
+++ src/components/ContentWrapper/ContentWrapper.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { Box, Typography } from '@material-ui/core';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import ExternalLink from '../ExternalLink/ExternalLink';
