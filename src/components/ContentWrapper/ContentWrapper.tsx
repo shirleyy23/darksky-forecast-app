84197cec6cc09dee23b8bae05eed3e810aa47dff
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-16 20:55:55 -0400

    added interface for mandatory props

--- src/components/ContentWrapper/ContentWrapper.tsx
+++ src/components/ContentWrapper/ContentWrapper.tsx
@@ -2,3 +2,7 @@ import React from 'react';
 import { Box, Typography } from '@material-ui/core';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import ExternalLink from '../ExternalLink/ExternalLink';
+
+interface Props {
+  children: React.ReactNode;
+}
