4a77fe41a09d516ebb7d3e7702d757ed9ebd7be9
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-17 20:34:56 -0400

    checked to see that app title renders

--- src/Pages/Home/__tests__/Home.test.tsx
+++ src/Pages/Home/__tests__/Home.test.tsx
@@ -1,3 +1,10 @@
 import React from 'react';
 import { render, fireEvent } from '../../../Store/Test/test-utils';
 import Home from '../Home';
+
+describe('<Home />', () => {
+  test('App title renders correctly', () => {
+    const { getByText } = render(<Home />);
+    expect(getByText('Weather App')).toBeInTheDocument();
+  });
+});
