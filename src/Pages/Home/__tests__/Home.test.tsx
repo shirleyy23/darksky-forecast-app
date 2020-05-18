15798e1f174ed80bcc47f84cdee321c538c112d2
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-17 20:45:40 -0400

    check that valid inputs allows form submission

--- src/Pages/Home/__tests__/Home.test.tsx
+++ src/Pages/Home/__tests__/Home.test.tsx
@@ -7,4 +7,17 @@ describe('<Home />', () => {
     const { getByText } = render(<Home />);
     expect(getByText('Weather App')).toBeInTheDocument();
   });
+
+  test('Valid inputs allow users to submit form', () => {
+    const { getByTestId } = render(<Home />);
+    const latitude = getByTestId('latitude');
+    const longitude = getByTestId('longitude');
+    fireEvent.change(latitude, { target: { value: 35.3134 } });
+    fireEvent.change(longitude, { target: { value: 24.9048 } });
+    expect(latitude).toHaveValue('35.3134');
+    expect(longitude).toHaveValue('24.9048');
+    const link = getByTestId('link');
+    expect(link).toHaveAttribute('href', '/forecast/35.3134,24.9048');
+    expect(link).not.toHaveClass('Mui-disabled Mui-disabled');
+  });
 });
