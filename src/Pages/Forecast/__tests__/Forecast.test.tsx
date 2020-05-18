3e4bd5ce98c24fb0384ba852ce28e63da58717de
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-17 21:15:33 -0400

    checked that correct elements load when API call is successful

--- src/Pages/Forecast/__tests__/Forecast.test.tsx
+++ src/Pages/Forecast/__tests__/Forecast.test.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { render, fireEvent } from '../../../Store/Test/test-utils';
+import {
+  render,
+  fireEvent,
+  waitFor,
+  screen,
+} from '../../../Store/Test/test-utils';
 import App from '../../../App';
 import axios from 'axios';
 const mockedAxios = axios as jest.Mocked<typeof axios>;
@@ -11,5 +16,14 @@ describe('<Forecast /> UI renders correctly based on API state', () => {
     fireEvent.click(button);
     expect(getByText('Loading...')).toBeInTheDocument();
     expect(getByText('Please wait as weather data loads')).toBeInTheDocument();
+  });
+  test('Forecast UI renders with API data', async () => {
+    const { getByTestId, getByText } = render(<App />);
+    const button = getByTestId('link');
+    fireEvent.click(button);
+    expect(getByText('Loading...')).toBeInTheDocument();
+    await waitFor(() => getByText('Forecast'));
+    const temperature = getByTestId('temperature');
+    expect(temperature.textContent).toBe('25.3℃');
   });
 });
