c956b2ac5f9b2a8f8e24b1a041a15e55faeb3a36
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-05-17 21:02:06 -0400

    added describe block

--- src/Pages/Forecast/__tests__/Forecast.test.tsx
+++ src/Pages/Forecast/__tests__/Forecast.test.tsx
@@ -3,3 +3,5 @@ import { render, fireEvent } from '../../../Store/Test/test-utils';
 import App from '../../../App';
 import axios from 'axios';
 const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+describe('<Forecast /> UI renders correctly based on API state', () => {});
