6c8788c542d1b9c735c8279303e64f29e3411265
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-06-07 16:23:45 -0400

    removed defined widths in styles

--- src/Pages/Forecast/Styles/Style.ts
+++ src/Pages/Forecast/Styles/Style.ts
@@ -3,13 +3,7 @@ import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 export const useStyles = makeStyles(({ palette }: Theme) =>
   createStyles({
     root: {
-      width: '60%',
       color: palette.secondary.main,
-    },
-    '@media (max-width: 680px)': {
-      root: {
-        width: '80%',
-      },
     },
   })
 );
