ecc49178bdbc5160eb4ef096d2b73fcb47f0c17f
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-06-07 15:01:04 -0400

    converted Form to class component and added default FullWidth from MUI

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Grid, TextField, Typography } from '@material-ui/core';
-import { useStyles } from './Styles/Styles';
 import CustomButton from '../CustomButton/CustomButton';
 import ContentWrapper from '../ContentWrapper/ContentWrapper';
 import { connect, ConnectedProps } from 'react-redux';
@@ -45,53 +44,51 @@ type PropsFromRedux = ConnectedProps<typeof connector>;
 
 type Props = PropsFromRedux;
 
-const Form: React.FC<Props> = (Props) => {
-  const {
-    longitude,
-    latitude,
-    getLocationInfo,
-    getAPIState,
-    updateFormSubmit,
-    isSubmitted,
-    data,
-  } = Props;
-
-  const classes = useStyles();
-
-  const [formError, setFormError] = useState({
+class Form extends React.Component<Props, FormState> {
+  constructor(props: Props) {
+    super(props);
+    this.handleError = this.handleError.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  state: FormState = {
     latitude: false,
     longitude: false,
-  });
+  };
 
-  const handleError = (value: string, name: string): boolean => {
+  handleError(value: string, name: string): any {
     if (!value || !+value) {
-      setFormError({
-        ...formError,
+      this.setState((state) => ({
+        ...this.state,
         [name]: true,
-      });
+      }));
       return false;
     }
-    setFormError({
-      ...formError,
-      [name]: false,
-    });
-    return true;
-  };
 
-  const handleInput = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    this.setState((state) => ({
+      ...this.state,
+      [name]: false,
+    }));
+    return true;
+  }
+
+  handleInput(e: React.ChangeEvent<HTMLInputElement>): void {
     const { value, name } = e.target;
-    handleError(value, name);
-    getLocationInfo({
-      ...Props,
+    this.handleError(value, name);
+    this.props.getLocationInfo({
+      ...this.props,
       [name]: value,
     });
-  };
+  }
 
-  const handleSubmit = async (): Promise<any> => {
-    updateFormSubmit({ isSubmitted: !isSubmitted });
-    getAPIState({ loading: true, success: false, fail: false });
+  async handleSubmit(): Promise<any> {
+    this.props.updateFormSubmit({ isSubmitted: !this.props.isSubmitted });
+    this.props.getAPIState({ loading: true, success: false, fail: false });
 
-    const submittedData = JSON.stringify({ latitude, longitude });
+    const submittedData = JSON.stringify({
+      latitude: this.props.latitude,
+      longitude: this.props.longitude,
+    });
 
     const apiData = axios.post('/.netlify/functions/weather', submittedData);
 
@@ -106,8 +103,8 @@ const Form: React.FC<Props> = (Props) => {
         windSpeed,
         uvIndex,
       } = response.data.data;
-      getLocationInfo({
-        ...Props,
+      this.props.getLocationInfo({
+        ...this.props,
         data: {
           timezone,
           summary,
@@ -118,66 +115,68 @@ const Form: React.FC<Props> = (Props) => {
           uvIndex,
         },
       });
-      getAPIState({ loading: false, success: true, fail: false });
+      this.props.getAPIState({ loading: false, success: true, fail: false });
     } catch (err) {
-      getAPIState({ loading: false, success: false, fail: true });
+      this.props.getAPIState({ loading: false, success: false, fail: true });
       console.error(err);
     }
-  };
+  }
 
-  return (
-    <ContentWrapper>
-      <Grid container spacing={5} component="form">
-        <Grid item xs={12}>
-          <Typography variant="subtitle1" component="p">
-            Enter the <strong>latitude</strong> and <strong>longitude</strong>
-            &nbsp;values of the location you would like to get weather
-            information for.
-          </Typography>
+  render(): React.ReactNode {
+    return (
+      <ContentWrapper>
+        <Grid container spacing={5} component="form">
+          <Grid item xs={12}>
+            <Typography variant="subtitle1" component="p">
+              Enter the <strong>latitude</strong> and <strong>longitude</strong>
+              &nbsp;values of the location you would like to get weather
+              information for.
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              error={this.state.latitude}
+              fullWidth
+              id="latitude"
+              label="Latitude"
+              value={this.props.latitude}
+              placeholder="-80.89343"
+              onChange={this.handleInput}
+              name="latitude"
+              inputProps={{
+                'data-testid': 'latitude',
+              }}
+              helperText={this.state.latitude && 'Please enter a number'}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              error={this.state.longitude}
+              fullWidth
+              id="longitude"
+              label="Longitude"
+              value={this.props.longitude}
+              placeholder="40.89722"
+              name="longitude"
+              onChange={this.handleInput}
+              inputProps={{
+                'data-testid': 'longitude',
+              }}
+              helperText={this.state.longitude && 'Please enter a number'}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <CustomButton
+              link={`/forecast/${this.props.latitude},${this.props.longitude}`}
+              content="SEARCH"
+              disabled={this.state.latitude || this.state.longitude}
+              onClick={this.handleSubmit}
+            />
+          </Grid>
         </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            error={formError.latitude}
-            className={classes.input}
-            id="latitude"
-            label="Latitude"
-            value={latitude}
-            placeholder="-80.89343"
-            onChange={handleInput}
-            name="latitude"
-            inputProps={{
-              'data-testid': 'latitude',
-            }}
-            helperText={formError.latitude && 'Please enter a number'}
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            error={formError.longitude}
-            className={classes.input}
-            id="longitude"
-            label="Longitude"
-            value={longitude}
-            placeholder="40.89722"
-            name="longitude"
-            onChange={handleInput}
-            inputProps={{
-              'data-testid': 'longitude',
-            }}
-            helperText={formError.longitude && 'Please enter a number'}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <CustomButton
-            link={`/forecast/${latitude},${longitude}`}
-            content="SEARCH"
-            disabled={formError.latitude || formError.longitude}
-            onClick={handleSubmit}
-          />
-        </Grid>
-      </Grid>
-    </ContentWrapper>
-  );
-};
+      </ContentWrapper>
+    );
+  }
+}
 
 export default connector(Form);
--- src/components/Form/Styles/Styles.ts
+++
@@ -1,9 +0,0 @@
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-
-export const useStyles = makeStyles(({ palette, spacing }: Theme) =>
-  createStyles({
-    input: {
-      width: '100%',
-    },
-  })
-);
