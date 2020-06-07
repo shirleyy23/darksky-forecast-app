9144c04a4e785b5b4cd1221d1695c0a996594680
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-06-07 16:50:17 -0400

    Forecast test to check for existence of weather-tab component

--- src/Pages/Forecast/__tests__/Forecast.test.tsx
+++ src/Pages/Forecast/__tests__/Forecast.test.tsx
@@ -15,26 +15,28 @@ afterEach(() => {
 
 describe('<Forecast /> UI renders correctly based on API state', () => {
   test('Loading UI renders correctly', () => {
-    const { getByTestId, getByText } = render(<App />);
+    const { getByTestId, getByText, queryByTestId } = render(<App />);
     const button = getByTestId('link');
     fireEvent.click(button);
     expect(getByText('Loading...')).toBeInTheDocument();
     expect(getByText('Please wait as weather data loads')).toBeInTheDocument();
+    expect(queryByTestId('forecast-tabs')).not.toBeInTheDocument();
   });
   test('Forecast UI renders with API data', async () => {
-    const { getByTestId, getByText } = render(<App />);
+    const { getByTestId, getByText, queryByTestId } = render(<App />);
     const button = getByTestId('link');
     fireEvent.click(button);
     expect(getByText('Loading...')).toBeInTheDocument();
     await waitFor(() => getByText('Forecast'));
     const temperature = getByTestId('temperature');
     expect(temperature.textContent).toBe('25.3℃');
+    expect(queryByTestId('forecast-tabs')).toBeInTheDocument();
   });
   test('Error UI renders correctly when API call is rejected', async () => {
     mockedAxios.post.mockImplementation(() =>
       Promise.reject({ payload: 'rejected' })
     );
-    const { getByTestId, getByText } = render(<App />);
+    const { getByTestId, getByText, queryByTestId } = render(<App />);
     const button = getByTestId('link');
     fireEvent.click(button);
     expect(getByText('Loading...')).toBeInTheDocument();
@@ -42,5 +44,6 @@ describe('<Forecast /> UI renders correctly based on API state', () => {
     expect(
       getByText('An error has occurred, and the data cannot be retrieved.')
     ).toBeInTheDocument();
+    expect(queryByTestId('forecast-tabs')).not.toBeInTheDocument();
   });
 });
