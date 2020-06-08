a587e843576f3c844da95c023fb216ca4867f542
Shirley Cao <32458216+shirleyy23@users.noreply.github.com>
2020-06-08 10:39:20 -0400

    added months and day arrays

--- src/Utilities.tsx
+++ src/Utilities.tsx
@@ -1,3 +1,5 @@
+import { FormattedDate } from './types';
+
 // Convert to percentage
 
 export const convertToPercentage = (value: number): string => {
@@ -11,3 +13,31 @@ export const convertWithUnit = (value: number, unit: string): string => {
   const formattedValue = `${value} ${unit}`;
   return formattedValue;
 };
+
+//Date arrays
+
+const days = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday',
+];
+
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
